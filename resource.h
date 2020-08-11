d9eeab6f8695ae885f4e6bcfe032ba6c8a8aafc7
Shahimian <shahimian@gmail.com>
2020-08-12 00:47:30 +0430

    add resources

---
+++ resource.h
@@ -0,0 +1,9 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDD_DIALOG1                             100
+#define IDR_MENU1                               101
+#define IDI_ICON1                               102
+#define IDOK                                    40000
+#define IDCANCEL                                40001
