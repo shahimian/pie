35c70e2e2bd09af9527db47872a639f5bae5348c
Shahimian <shahimian@gmail.com>
2020-08-12 00:59:25 +0430

    add icon in main script

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <windows.h>
+#include <tchar.h>
+#include "resource.h"
 
 /* This is where all the input to the window goes to */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
@@ -19,6 +21,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) 
 
 /* The 'main' function of Win32 GUI programs: this is where execution starts */
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	LPTSTR szClassName = _T("PIE");
 	WNDCLASSEX wc; /* A properties struct of our window */
 	HWND hwnd; /* A 'HANDLE', hence the H, or a pointer to our window */
 	MSG msg; /* A temporary location for all messages */
@@ -32,16 +35,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	
 	/* White, COLOR_WINDOW is just a #define for a system color, try Ctrl+Clicking it */
 	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
-	wc.lpszClassName = "WindowClass";
-	wc.hIcon		 = LoadIcon(NULL, IDI_APPLICATION); /* Load a standard icon */
-	wc.hIconSm		 = LoadIcon(NULL, IDI_APPLICATION); /* use the name "A" to use the project icon */
+	wc.lpszClassName = szClassName;
+	wc.hIcon		 = LoadIcon(NULL, MAKEINTRESOURCE(IDI_ICON1)); /* Load a standard icon */
+	wc.hIconSm		 = LoadIcon(NULL, MAKEINTRESOURCE(IDI_ICON1)); /* use the name "A" to use the project icon */
 
 	if(!RegisterClassEx(&wc)) {
 		MessageBox(NULL, "Window Registration Failed!","Error!",MB_ICONEXCLAMATION|MB_OK);
 		return 0;
 	}
 
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Caption",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
+	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,szClassName,_T("Pie 1.0"),WS_VISIBLE|WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, /* x */
 		CW_USEDEFAULT, /* y */
 		640, /* width */
--- resource.h
+++ resource.h
@@ -5,5 +5,5 @@
 #define IDD_DIALOG1                             100
 #define IDR_MENU1                               101
 #define IDI_ICON1                               102
-#define IDOK                                    40000
-#define IDCANCEL                                40001
+#define IDOK1                                   40000
+#define IDCANCEL1                               40001
